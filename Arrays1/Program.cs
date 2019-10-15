691dd1b49d54d57200b66451eade2d9785ee7bce
Yaroslav <yaroslavflash@gmail.com>
2019-10-15 17:11:02 +0300

    1comit

---
+++ Arrays1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кількість елементів масиву - ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int index;
+            double[] Arr = new double[N];
+            Random r = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                Arr[i] = (r.Next(-1000, 1001)) / 10.0;
+                Console.Write(Arr[i] + "  ");
+            }
+            Console.Write("\n");
+            Console.WriteLine("сумма: {0}", sumless0(N, Arr));
+            Console.WriteLine("MAX: {0} Індекс: {1}", maxandindex(N, Arr, out index), index);
+            Console.WriteLine("MAX за модулем: {0}", maxabs(N, Arr));
+            Console.WriteLine("Сума індексів: {0}", sumindex(N, Arr));
+            Console.WriteLine("Кількість цілих чисел: {0}", countindex(N, Arr));
+            Console.ReadKey();
+        }
+        static double sumless0(int N, double[] Arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] < 0)
+                {
+                    sum += Arr[i];
+                }
+            }
+            return sum;
+        }
+        static double maxandindex(int N, double[] Arr, out int index)
+        {
+            double max = -100;
+            index = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] > max)
+                {
+                    max = Arr[i];
+                    index = i;
+                }
+            }
+            return max;
+        }
+        static double maxabs(int N, double[] Arr)
+        {
+            double maxabs = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Math.Abs(Arr[i]) > Math.Abs(maxabs))
+                {
+                    maxabs = Arr[i];
+                }
+            }
+            return maxabs;
+        }
+        static int sumindex(int N, double[] Arr)
+        {
+            int sumindex = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] > 0)
+                {
+                    sumindex += i;
+                }
+            }
+            return sumindex;
+        }
+        static int countindex(int N, double[] Arr)
+        {
+            int indexcount = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] % 1 == 0)
+                {
+                    indexcount++;
+                }
+            }
+            return indexcount;
+        }
+    }
+}
---
+++ Arrays2/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Кількість елементів масиву - ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            double[] Arr = new double[N];
+            Random r = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                Arr[i] = (r.Next(-1000, 1001)) / 10.0;
+                Console.Write(Arr[i] + "  ");
+            }
+            Console.Write("\n");
+            Console.WriteLine("Множення після мінімального: {0}", multiple(N, Arr));
+            Console.WriteLine("Дивне додавання: {0}", StrangeSum(N, Arr));
+            Console.WriteLine("Дивне додавання2: {0}", StrangeSum2(N, Arr));
+            Console.WriteLine("Дивне множення: {0}", maxabsminabs(N, Arr));
+        }
+        static double multiple (int N, double[] Arr)
+        {
+            double multiple = 1;
+            double min = 0;
+            int index=0;
+            for (int i = 0;i < N; i++)
+            {
+                if (Arr[i]< min)
+                {
+                    min = Arr[i];
+                    index = i+1;
+                }
+            }
+            if (index >= N)
+            {
+                return 0;
+            }
+            else
+            {
+                for (; index < N; index++)
+                {
+                        multiple *= Arr[index];
+                }
+            }
+            return multiple;
+        }
+        static double StrangeSum(int N, double[] Arr)
+        {
+            double StrangeSum = 0;
+            int indexmin = 0;
+            int k = 0;
+            int indexmax = N;
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] < 0)
+                {
+                    indexmin = i + 1;
+                    break;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] > 0)
+                {
+                    k++;
+                }
+                if (k==2)
+                {
+                    indexmax = i;
+                    break;
+                }
+            }
+            if (indexmin > indexmax)
+            {
+                int o = indexmin;
+                indexmin = indexmax + 1;
+                indexmax = o - 1;
+            }
+            for (; indexmin < indexmax; indexmin++)
+                StrangeSum += Arr[indexmin];
+            return StrangeSum;
+        }
+        static double StrangeSum2(int N, double[] Arr)
+        {
+            int min = 0, max = N, k = 0;
+            double sum = 0;
+            for (int i = 0; i < N; i++)
+                if (Arr[i] == 0)
+                {
+                    k++;
+                    switch (k)
+                    {
+                        case 1: min = i + 1; break;
+                        case 2: max = i; break;
+                    }
+                }
+            if (min == 0 || max == N)
+            {
+                Console.WriteLine("Від 0 до останнього елемента");
+            }
+            if (min > max)
+            {
+                int o = min;
+                min = max + 1;
+                max = o;
+            }
+            for (; min < max; min++)
+                sum += Arr[min];
+            return sum;
+        }
+        static double maxabsminabs(int N, double[] Arr)
+        {
+            double m=1;
+            double maxabs = Double.MaxValue;
+            int indexmax=N;
+            int indexmin = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (Math.Abs(Arr[i]) > Math.Abs(maxabs))
+                {
+                    maxabs = Arr[i];
+                    indexmax = i;
+                }
+            }
+            double minabs = Double.MinValue;
+            for (int i = 0; i < N; i++)
+            {
+                if (Math.Abs(Arr[i]) < Math.Abs(minabs))
+                {
+                    minabs = Arr[i];
+                    indexmin = i + 1;
+                }
+            }
+            if (indexmin > indexmax)
+            {
+                int o = indexmin;
+                indexmin = indexmax + 1;
+                indexmin = o - 1;
+            }
+            for (;indexmin<indexmax;indexmin++)
+            {
+                m = Arr[indexmin];
+            }
+            return m;
+        }
+    }
+}
