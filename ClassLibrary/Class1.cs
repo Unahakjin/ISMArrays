57d587a7c1657356b3066c5fe5a887e6f3beb5a0
Yaroslav <yaroslavflash@gmail.com>
2019-11-07 15:38:22 +0200

    OOPDraw
    OOPDraw

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ClassLibrary
+{
+   public abstract class ShapePoint
+   {
+        protected Color color;
+        protected int x1;
+        protected int y1;
+        public ShapePoint(Color color, int x, int y)
+        {
+            this.color = color;
+            x1 = x;
+            y1 = y;
+        }
+        public abstract void Draw(Graphics g);
+   }
+    public class Dot : ShapePoint
+    {
+        public Dot(Color color, int x, int y) : base(color, x, y)
+        {
+
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Brush brush = new SolidBrush(color);
+            g.FillEllipse(brush, x1 - 1, y1 - 1, 2, 2);
+        }
+    }
+    public class Line : Dot
+    {
+        protected int x2;
+        protected int y2;
+        public Line(Color color, int x1, int y1, int x2, int y2) : base(color, x1, y1)
+        {
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(color, 5);
+            g.DrawLine(pen, x1, y1, x2, y2);
+        }
+    }
+    public class Rectangle : Line
+    {
+        protected int width;
+        protected int height;
+        public Rectangle(Color color, int x1, int y1, int width, int height) : base(color, x1, y1, width, height)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(color, 3);
+            g.DrawRectangle(pen, x1, y1, width, height);
+        }
+    }
+    public class Circle : Dot
+    {
+        protected int rad;
+        public Circle(Color color, int x1, int y1, int rad) : base(color, x1, y1)
+        {
+            this.rad = rad;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(color, 5);
+            g.DrawEllipse(pen, x1 - 1, y1 - 1, rad, rad);
+        }
+    }
+    public class Ellipse : Dot
+    {
+        protected int rad1;
+        protected int rad2;
+        public Ellipse(Color color, int x1, int y1, int rad1, int rad2) : base(color, x1, y1)
+        {
+            this.rad1 = rad1;
+            this.rad2 = rad2;
+        }
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(color, 5);
+            g.DrawEllipse(pen, x1 - 1, y1 - 1, rad1, rad2);
+        }
+    }
+}
---
+++ OOPDraw/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace OOPDraw
+{
+    public partial class Form1 : Form
+    {
+            Bitmap canvas;
+            public Form1()
+            {
+                InitializeComponent();
+                canvas = new Bitmap(1920, 1080);
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Button1_Click(object sender, EventArgs e)
+            {
+                Graphics g = Graphics.FromImage(canvas);
+                //Graphics g = pictureBox1.CreateGraphics();
+                ShapePoint[] shapes = new ShapePoint[10];
+                Random randome = new Random();
+                for (int i = 0; i < shapes.Length; i++)
+                {
+                    int q = randome.Next(1, 6);
+                    switch (q)
+                    {
+                        case 1:
+                            Dot dot = new Dot(Color.FromArgb(randome.Next(0, 256), randome.Next(0, 256), randome.Next(0, 256)),randome.Next(0,1920), randome.Next(0,1080));
+                            dot.Draw(g);
+                            pictureBox1.Image = canvas;
+                            break;
+                        case 2:
+                            Line line = new Line(Color.FromArgb(randome.Next(0, 256), randome.Next(0, 256), randome.Next(0, 256)), randome.Next(0, pictureBox1.Width), randome.Next(0, pictureBox1.Height), randome.Next(0, pictureBox1.Width), randome.Next(0, pictureBox1.Height));
+                            line.Draw(g);
+                            pictureBox1.Image = canvas;
+                            break;
+                        case 3:
+                        ClassLibrary.Rectangle rectangle = new ClassLibrary.Rectangle(Color.FromArgb(randome.Next(0, 256), randome.Next(0, 256), randome.Next(0, 256)), randome.Next(0, pictureBox1.Width), randome.Next(0, pictureBox1.Height), randome.Next(0, pictureBox1.Width/2), randome.Next(0, pictureBox1.Height/2));
+                            rectangle.Draw(g);
+                            pictureBox1.Image = canvas;
+                            break;
+                        case 4:
+                            Circle circle = new Circle(Color.FromArgb(randome.Next(0, 256), randome.Next(0, 256), randome.Next(0, 256)), randome.Next(0, 1920), randome.Next(0, 1080),randome.Next(0,500));
+                            circle.Draw(g);
+                            pictureBox1.Image = canvas;
+                            break;
+                        case 5:
+                            Ellipse ellipse = new Ellipse(Color.FromArgb(randome.Next(0, 256), randome.Next(0, 256), randome.Next(0, 256)), randome.Next(0, 1920), randome.Next(0, 1080), randome.Next(0, 500), randome.Next(0, 500));
+                            ellipse.Draw(g);
+                            pictureBox1.Image = canvas;
+                            break;
+                }
+                }
+            }
+
+            private void Button2_Click(object sender, EventArgs e)
+            {
+                Graphics g = Graphics.FromImage(canvas);
+                g.Clear(Color.White);
+                pictureBox1.Image = canvas;
+            }
+    }
+}
+   
